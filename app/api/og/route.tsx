cdc2743dc164c67b4b58b7f53b3bf516caff37c5
Chris Tate <chris@ctate.dev>
2025-01-20 11:58:53 -0600

    fixes geist sans in og

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,23 +1,30 @@
 import { ImageResponse } from "next/og";
-import { GeistSans } from "geist/font/sans";
 
 export const runtime = "edge";
+
+async function loadGoogleFont(font: string, text: string) {
+  const url = `https://fonts.googleapis.com/css2?family=${font}&text=${encodeURIComponent(text)}`
+  const css = await (await fetch(url)).text()
+  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/)
+
+  if (resource) {
+    const response = await fetch(resource[1])
+    if (response.status === 200) {
+      return await response.arrayBuffer()
+    }
+  }
+
+  throw new Error('failed to load font data')
+}
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const title = searchParams.get("title") || "OpenUI";
   const description =
-    searchParams.get("description") || "AI-Native UI Standardization";
+    searchParams.get("description") || "AI-Native Specification for UIs";
 
   const logoUrl = new URL("/logo.png", request.url).toString();
   const logoData = await fetch(logoUrl).then((res) => res.arrayBuffer());
-
-  const fontData = await fetch(
-    new URL(
-      "node_modules/geist/dist/fonts/geist-sans/Geist-Regular.woff",
-      import.meta.url,
-    ),
-  ).then((res) => res.arrayBuffer());
 
   return new ImageResponse(
     (
@@ -43,7 +50,7 @@ export async function GET(request: Request) {
         />
         <div
           style={{
-            marginBottom: 40,
+            marginBottom: 20,
             fontSize: 60,
             textAlign: "center",
             maxWidth: "90%",
@@ -67,10 +74,9 @@ export async function GET(request: Request) {
       height: 630,
       fonts: [
         {
-          name: "Geist Sans",
-          data: fontData,
-          style: "normal",
-          weight: 400,
+          name: 'Geist',
+          data: await loadGoogleFont('Geist', `${title} - ${description}`),
+          style: 'normal',
         },
       ],
     },
