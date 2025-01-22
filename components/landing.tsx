e895f39e29ee6b83395654b0cb144d378b964b40
Chris Tate <chris@ctate.dev>
2025-01-22 00:12:21 -0600

    simplifies landing page

--- components/landing.tsx
+++ components/landing.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { SpecExamples } from "./spec-examples";
-import { GoalsSection } from "./goals-section";
-import { DetailedIntroSection } from "./detailed-intro-section";
 import { HeroSection } from "./hero-section";
 import { FeaturesGrid } from "./features-grid";
 import { CallToAction } from "./call-to-action";
@@ -13,8 +11,6 @@ export default function OpenUILandingPage() {
       <div className="container mx-auto px-4 py-12 space-y-24">
         <SpecExamples />
         <FeaturesGrid />
-        <GoalsSection />
-        <DetailedIntroSection />
         <CallToAction />
       </div>
     </div>
